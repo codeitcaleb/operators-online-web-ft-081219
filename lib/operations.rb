9e47326ff7ac288ea72ea812c8ea16a6a29e8baf
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-13 14:57:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,9 @@
 #unsafe? returns false if the speed is between 40 and 60
 
 def unsafe?(speed)
- unsafe? >= 40 || 60
+ unsafe? = 
  
-  if unsafe? == true 
+  if unsafe? > 60 || <
     puts true
   else puts false
  end
